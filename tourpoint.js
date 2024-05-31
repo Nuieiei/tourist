54fbad532eedfba695d121cf1e84fa9fff79b877
Nuieiei <158017511+Nuieiei@users.noreply.github.com>
2024-05-31 23:56:46 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,156 @@
+var tourpoint =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "อุทยานหินเขางู",
+        "Province": "ราชบุรี",
+        "Type": "natural"
+      },
+      "geometry": {
+        "coordinates": [
+          99.77461745818329,
+          13.56913872410216
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "ถ้ำเขาบิน",
+        "Province": "ราชบุรี",
+        "Type": "natural"
+      },
+      "geometry": {
+        "coordinates": [
+          99.66680427116103,
+          13.592220452449254
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "วัดคงคาราม",
+        "Province": "ราชบุรี",
+        "Type": "cultural"
+      },
+      "geometry": {
+        "coordinates": [
+          99.8507172711636,
+          13.71558037171259
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "วัดป่าพุทธาราม (ถ้ำสติ )",
+        "Province": "ราชบุรี",
+        "Type": "cultural"
+      },
+      "geometry": {
+        "coordinates": [
+          99.75872300000043,
+          13.701638372742522
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Alpaca Hill",
+        "Province": "ราชบุรี",
+        "Type": "natural"
+      },
+      "geometry": {
+        "coordinates": [
+          99.20382199999966,
+          13.709581795323544
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "บ้านหอมเทียน",
+        "Province": "ราชบุรี",
+        "Type": "Special "
+      },
+      "geometry": {
+        "coordinates": [
+          99.31426399999935,
+          13.544671814723984
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "เขากระโจม",
+        "Province": "ราชบุรี",
+        "Type": "natural"
+      },
+      "geometry": {
+        "coordinates": [
+          99.17950957671667,
+          13.582099696421167
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "The Scenery Vintage Farm",
+        "Province": "ราชบุรี",
+        "Type": "natural"
+      },
+      "geometry": {
+        "coordinates": [
+          99.28962599999733,
+          13.509915818785103
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "วัดขนอนหนังใหญ่",
+        "Province": "ราชบุรี",
+        "Type": "cultural"
+      },
+      "geometry": {
+        "coordinates": [
+          99.84475278042447,
+          13.726748845018037
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "กาดวิถีชุมชนคูบัว",
+        "Province": "ราชบุรี",
+        "Type": "cultural"
+      },
+      "geometry": {
+        "coordinates": [
+          99.83570727116495,
+          13.486808687429686
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
